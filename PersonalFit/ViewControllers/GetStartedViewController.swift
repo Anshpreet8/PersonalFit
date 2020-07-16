0d017d7a1bc8f1aef2357e2afbd0d38a06b7d1fb
Anshpreet <anshpreet5678@github.com>
2020-07-16 18:15:19 -0400

    designed the home view controller

---
+++ PersonalFit/ViewControllers/GetStartedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GetStartedViewController.swift
+//  PersonalFit
+//
+//  Created by Anshpreet Sandhu on 2020-07-16.
+//  Copyright © 2020 Anshpreet Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ PersonalFit/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalFit
+//
+//  Created by Anshpreet Sandhu on 2020-07-14.
+//  Copyright © 2020 Anshpreet Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
