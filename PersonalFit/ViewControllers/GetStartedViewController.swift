03de61003e938ae6be6da9570d8e9cd73d7d2018
Anshpreet <anshpreet5678@github.com>
2020-07-18 11:39:14 -0400

    added the timer design to the getStartedViewController

--- PersonalFit/ViewControllers/GetStartedViewController.swift
+++ PersonalFit/ViewControllers/GetStartedViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class GetStartedViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var TimerDisplayed = 0
+    
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func startButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
