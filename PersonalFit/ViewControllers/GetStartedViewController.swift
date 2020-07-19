23b70e2377205ffff29b387438e8c75451be222f
Anshpreet <anshpreet5678@github.com>
2020-07-18 21:07:50 -0400

    added a timer and a text view field

--- PersonalFit/ViewControllers/GetStartedViewController.swift
+++ PersonalFit/ViewControllers/GetStartedViewController.swift
@@ -11,12 +11,46 @@ import UIKit
 class GetStartedViewController: UIViewController {
     
     var timer = Timer()
-    var TimerDisplayed = 0
+    var timerDisplayed = 30
+    var whichTimer = 0 // will either be 1 or 2
+    var numTimes = 1 // how many times the timer has run
     
     
     @IBOutlet weak var timerLabel: UILabel!
     
     @IBAction func startButton(_ sender: Any) {
+        
+        // timer interval: how much it will be repeated (every 1 sec)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func Action(){
+        
+        timerDisplayed -= 1
+        timerLabel.text = String(timerDisplayed)
+        
+        if (timerDisplayed == -1 && whichTimer == 0){
+            timerDisplayed = 11
+            timerDisplayed -= 1
+            timerLabel.text = String(timerDisplayed)
+            whichTimer += 1
+        } else if (timerDisplayed == -1 && whichTimer == 1) {
+            timerDisplayed = 31
+            timerDisplayed -= 1
+            timerLabel.text = String(timerDisplayed)
+            whichTimer -= 1
+            numTimes += 1
+        }
+        
+        if (numTimes == 6){
+            timer.invalidate()
+            numTimes = 0
+        }
+        
+        
+        // to stop: timer.invalidate
+        
     }
     
     override func viewDidLoad() {
